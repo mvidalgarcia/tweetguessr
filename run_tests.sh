d826711f1eca0e5af97b302e75e06402e795c729
Marco Vidal García <marcovidal67@gmail.com>
2015-05-13 16:04:40 +0200

    Script for running tests

---
+++ run_tests.sh
@@ -0,0 +1,34 @@
+python3 tweetguessr/tweetguessr.py
+python3 tweetguessr/tweetguessr.py
+python3 tweetguessr/tweetguessr.py
+python3 tweetguessr/tweetguessr.py
+
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.4
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.4
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.4
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.4
+
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.3
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.3
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.3
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.3
+
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.25
+
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.2
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.2
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.2
+python3 tweetguessr/tweetguessr.py --llr-threshold 0.2
+
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5
+
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25
+python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25
