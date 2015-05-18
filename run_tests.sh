7e182dd1300e08d2a14fe140b7f3586bb39e6ed0
Marco Vidal García <marcovidal67@gmail.com>
2015-05-18 23:16:21 +0200

    run_test.sh has proper names

--- run_tests.sh
+++ run_tests.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
-python3 tweetguessr/tweetguessr.py --write-results
-python3 tweetguessr/tweetguessr.py --write-results
-python3 tweetguessr/tweetguessr.py --write-results
-python3 tweetguessr/tweetguessr.py --write-results
+python3 run.py --write-results
+python3 run.py --write-results
+python3 run.py --write-results
+python3 run.py --write-results
 
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.4 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.4 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.4 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.4 --write-results
+python3 run.py --llr-threshold 0.4 --write-results
+python3 run.py --llr-threshold 0.4 --write-results
+python3 run.py --llr-threshold 0.4 --write-results
+python3 run.py --llr-threshold 0.4 --write-results
 
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.3 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.3 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.3 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.3 --write-results
+python3 run.py --llr-threshold 0.3 --write-results
+python3 run.py --llr-threshold 0.3 --write-results
+python3 run.py --llr-threshold 0.3 --write-results
+python3 run.py --llr-threshold 0.3 --write-results
 
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.25 --write-results
+python3 run.py --llr-threshold 0.25 --write-results
+python3 run.py --llr-threshold 0.25 --write-results
+python3 run.py --llr-threshold 0.25 --write-results
+python3 run.py --llr-threshold 0.25 --write-results
 
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.2 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.2 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.2 --write-results
-python3 tweetguessr/tweetguessr.py --llr-threshold 0.2 --write-results
+python3 run.py --llr-threshold 0.2 --write-results
+python3 run.py --llr-threshold 0.2 --write-results
+python3 run.py --llr-threshold 0.2 --write-results
+python3 run.py --llr-threshold 0.2 --write-results
 
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.5 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.5 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.5 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.5 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.5 --write-results
 
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25 --write-results
-python3 tweetguessr/tweetguessr.py --min-conf 0.8 --llr-threshold 0.25 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.25 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.25 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.25 --write-results
+python3 run.py --min-conf 0.8 --llr-threshold 0.25 --write-results
